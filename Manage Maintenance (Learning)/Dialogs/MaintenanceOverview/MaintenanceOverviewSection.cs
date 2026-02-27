a538493e3460fc4e6a4f8a4de76a824cabbcdf4c
Thomas <thomas.remmery@skyline.be>
2026-02-27 15:24:09 +0100

    Minor rework, use best practices and define the methods to add/edit/delete maintenance windows in the script entry class.

--- Manage Maintenance (Learning)/Dialogs/MaintenanceOverview/MaintenanceOverviewSection.cs
+++ Manage Maintenance (Learning)/Dialogs/MaintenanceOverview/MaintenanceOverviewSection.cs
@@ -1,44 +1,32 @@
 ﻿namespace Skyline.DataMiner.Learning.MakingMaintenanceObservable.ManageMaintenance.Dialogs.MaintenanceOverview
 {
-	using DeviceMaintenanceApi.Models;
-	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
-	using System.Collections.Generic;
 	using System;
+	using System.Collections.Generic;
+
+	using DeviceMaintenanceApi.Models;
+
+	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
 
 	public class MaintenanceOverviewSection : Section
 	{
-		public Section Header;
-
-		public event Action AddMaintenance;
-
-		public event Action<MaintenanceWindow> EditMaintenance;
-
-		public event Action<MaintenanceWindow> DeleteMaintenance;
+		private Section header;
 
 		public MaintenanceOverviewSection()
 		{
 			InitializeHeader();
 		}
 
-		private void InitializeHeader()
-		{
-			var addButton = new Button("Add");
-			addButton.Width = 60;
-			addButton.Pressed += (sender, args) => AddMaintenance?.Invoke();
+		public event EventHandler AddMaintenance;
 
-			Header = new Section()
-				.AddWidget(new Label("Start") { Width = 180, Style = TextStyle.Bold } , 0, 0)
-				.AddWidget(new Label("End") { Width = 180, Style = TextStyle.Bold }, 0, 1)
-				.AddWidget(new Label("Impact") { Width = 80, Style = TextStyle.Bold }, 0, 2)
-				.AddWidget(new Label("Description") { Width = 250, Style = TextStyle.Bold }, 0, 3)
-				.AddWidget(addButton, 0, 4);
-		}
+		public event EventHandler<MaintenanceWindow> EditMaintenance;
+
+		public event EventHandler<MaintenanceWindow> DeleteMaintenance;
 
 		public void Load(IReadOnlyCollection<MaintenanceWindow> maintenanceWindows)
 		{
 			Clear();
 			var row = 0;
-			AddSection(Header, row++, 0);
+			AddSection(header, row++, 0);
 
 			if (maintenanceWindows.Count == 0)
 			{
@@ -50,9 +38,22 @@
 			{
 				var maintenanceSection = new MaintenanceSection(maintenanceWindow);
 				AddSection(maintenanceSection, row++, 0);
-				maintenanceSection.Edit += () => EditMaintenance?.Invoke(maintenanceWindow);
-				maintenanceSection.Delete += () => DeleteMaintenance?.Invoke(maintenanceWindow);
+				maintenanceSection.Edit += (sender, args) => EditMaintenance?.Invoke(this, args);
+				maintenanceSection.Delete += (sender, args) => DeleteMaintenance?.Invoke(this, args);
 			}
+		}
+
+		private void InitializeHeader()
+		{
+			var addButton = new Button("Add") { Width = 60 };
+			addButton.Pressed += (sender, args) => AddMaintenance?.Invoke(this, EventArgs.Empty);
+
+			header = new Section()
+				.AddWidget(new Label("Start") { Width = 180, Style = TextStyle.Bold }, 0, 0)
+				.AddWidget(new Label("End") { Width = 180, Style = TextStyle.Bold }, 0, 1)
+				.AddWidget(new Label("Impact") { Width = 80, Style = TextStyle.Bold }, 0, 2)
+				.AddWidget(new Label("Description") { Width = 250, Style = TextStyle.Bold }, 0, 3)
+				.AddWidget(addButton, 0, 4);
 		}
 	}
 }
